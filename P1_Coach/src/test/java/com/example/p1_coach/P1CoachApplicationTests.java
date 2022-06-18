0e9893bb81fef9a7e2225a9b80c77a0584f189f7
johnhatem <>
2022-06-18 13:02:40 +0400

    update main gitignore, initialize first project in the course

---
+++ P1_Coach/src/test/java/com/example/p1_coach/P1CoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p1_coach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1CoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
